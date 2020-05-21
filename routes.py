f2ddfb2703690133443791dbbfc9c5f387bc7c0c
Ivy Allie <ivy@ivyallie.com>
2020-05-21 18:55:15 -0400

    Basic index display

--- routes.py
+++ routes.py
@@ -1,17 +1,32 @@
 from flask import render_template, Blueprint
+from . import db
+from json import loads
 
 bp = Blueprint('routes', __name__)
-from . import db
+
 database = db.Database()
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    comics_raw = database.query("""SELECT * FROM comic;""")
+    print(comics_raw)
+    comics=[]
+    for comic in comics_raw:
+        body_raw=str(comic['body']).replace('\'', '\"')
+        body=loads(body_raw)
+        tags = body['tags']
+        d = {
+            'title':comic['title'],
+            'body':body['body_text'],
+            'tags':tags
+        }
+        comics.append(d)
+    return render_template('index.html', comics=comics)
 
 
 @bp.route('/beepers/')
 def sql_test():
 
-    beepers = database.query("""SELECT * FROM example""")
+    beepers = database.query("""SELECT * FROM example;""")
 
     return render_template('sql_test.html', beepers=beepers)

8f908cb7e8db256baab27dc749bb3950126604b5
Ivy Allie <ivy@ivyallie.com>
2020-07-07 17:15:15 -0400

    Homepage editor can now post changes

--- static/collection_editor.js
+++ static/collection_editor.js
@@ -11,13 +11,17 @@ let comic_menu = document.getElementById('comic_menu');
                     //console.log(result['comics']);
                     let comics = result['comics'];
                     generateMenu(comics);
-                    console.log('Comics:', comics);
+                    //console.log('Comics:', comics);
                 })
                 .catch(error => {
                     console.log('Error:', error);
                 })
 
         };
+
+        let getUniqueId = function() {
+            return new Date().getTime();
+        }
 
         let listCollections = function() {
             fetch('/getcollections', {
@@ -51,6 +55,8 @@ let comic_menu = document.getElementById('comic_menu');
             let clone = listing_template.content.cloneNode(deep = true,);
             let name = clone.querySelector('span');
             let viewlink = clone.querySelector('a');
+            let how_many = clone.querySelector('div.how_many');
+            let expand_option = clone.querySelector('div.expand_option');
             name.innerHTML = display_title;
             name.setAttribute('title', internal_title);
             if (link) {
@@ -60,24 +66,53 @@ let comic_menu = document.getElementById('comic_menu');
             }
 
             if (extra_class) {
-                console.log(extra_class)
+                //console.log(extra_class);
                 let li = clone.querySelector('li');
-                li.classList.add(extra_class)
+                li.classList.add(extra_class);
+                if (extra_class==='collection') {
+                    how_many.remove();
+                    setUniqueIDs(expand_option);
+                } else if (extra_class==='smartlist') {
+                    expand_option.remove();
+                    setUniqueIDs(how_many);
+                } else {
+                    if (expand_option) {
+                        expand_option.remove();
+                    }
+                    if (how_many) {
+                        how_many.remove();
+                    }
+
+                }
+            } else {
+                if (how_many) {
+                    how_many.remove();
+                }
+                if (expand_option) {
+                    expand_option.remove();
+                }
+
             }
 
             comic_menu.appendChild(clone)
 
         };
 
+        let setUniqueIDs = function(parent) {
+            let id=getUniqueId();
+            parent.querySelector('input').setAttribute('id',id);
+            parent.querySelector('label').setAttribute('for',id);
+        };
+
         let listSmartSections = function() {
             let types = [
-                {'name':'Recent comics','internal_name':'recent_comics'},
-                {'name':'Recent collections','internal_name':'recent_collections'},
-                {'name':'About','internal_name':'about'},
+                {'name':'Recent comics','internal_name':'recent_comics','class':'smartlist'},
+                {'name':'Recent collections','internal_name':'recent_collections','class':'smartlist'},
+                {'name':'About','internal_name':'about','class':'smart'},
             ];
             comic_menu.innerHTML='';
             for (i=0; i<types.length; i++) {
-                createMenuEntry(types[i]['name'],types[i]['internal_name'], link="",extra_class="smart")
+                createMenuEntry(types[i]['name'],types[i]['internal_name'], link="",extra_class=types[i]['class'])
             }
         };
 
@@ -93,7 +128,7 @@ let comic_menu = document.getElementById('comic_menu');
             let title = document.getElementById('title');
             let description = document.getElementById('description');
             let sequence = getComicSequence();
-            console.log(sequence);
+            //console.log(sequence);
             let submit_button = document.getElementById('submit_button');
             let collection_id = null;
             if (submit_button.getAttribute('data-id')) {
@@ -131,6 +166,77 @@ let comic_menu = document.getElementById('comic_menu');
                     alert(error);
                 })
 
+        };
+
+        let postHomepage = function() {
+            let sequence=getHomepageSequence();
+            let sequence_json = JSON.stringify(sequence);
+            //console.log(sequence);
+            fetch('/admin/homepage/modify', {
+                method:'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: sequence_json,
+            })
+                 .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Something went wrong');
+                    }
+                    return response.json();
+                })
+                .then(result => {
+                    //let redirect = result['redirect'];
+                    //window.location.replace(redirect);
+                    alert('Successfully updated homepage!')
+                })
+                .catch((error) => {
+
+                    alert(error);
+                })
+        };
+
+        let getHomepageSequence = function() {
+            let collection_sequence = document.getElementById('collection_sequence');
+            let list_elements = collection_sequence.querySelectorAll('li');
+            let homepage_sequence = [];
+            for (i=0; i<list_elements.length; i++) {
+                let classes = list_elements[i].classList;
+                let title = list_elements[i].querySelector('span').getAttribute('title');
+
+                if (classes.contains('collection')) {
+                    let expand = list_elements[i].querySelector('div.expand_option input:checked') !== null;
+                    let collection = {
+                        'title':title,
+                        'type':'collection',
+                        'expand':expand
+                    };
+                    homepage_sequence.push(collection)
+                } else if (classes.contains('smartlist')) {
+                    let how_many = list_elements[i].querySelector('div.how_many input').value;
+                    let smartlist = {
+                        'title':title,
+                        'type':'smartlist',
+                        'how_many':how_many
+                    };
+                    homepage_sequence.push(smartlist);
+                } else if (classes.contains('smart')) {
+                    let smart = {
+                        'title':title,
+                        'type':'smart'
+                    };
+                    homepage_sequence.push(smart);
+                } else {
+                    let comic = {
+                        'title':title,
+                        'type':'comic'
+                    };
+                    homepage_sequence.push(comic);
+                }
+            }
+            return homepage_sequence
+        };
+
+        let inList = function(str,list) {
+            return ((list.indexOf(str))>=0)
         };
 
         let getComicSequence = function () {
